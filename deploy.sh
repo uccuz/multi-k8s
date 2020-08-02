3fa6bcb0e812cbe36c83da1a8880fcf5388b8cdc
林奕宏 (107504511) <justin89721@office365.ncu.edu.tw>
2020-08-02 23:14:39 +0800

    add travis.yml

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t justin89721/multi-client:latest -t justin89721/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t justin89721/multi-server:latest -t justin89721/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t justin89721/multi-worker:latest -t justin89721/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push justin89721/multi-client:latest
+docker push justin89721/multi-server:latest
+docker push justin89721/multi-worker:latest
+
+docker push justin89721/multi-client:$SHA
+docker push justin89721/multi-server:$SHA
+docker push justin89721/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=justin89721/multi-server:$SHA
+kubectl set image deployments/client-deployment server=justin89721/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=justin89721/multi-worker:$SHA
